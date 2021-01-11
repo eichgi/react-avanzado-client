749fb577b5f60d8b2d39d9cfd226bd368de4215b
Hiram Guerrero <hiramg90@gmail.com>
2021-01-11 12:17:12 -0600

    initial setup

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,10 +1,10 @@
 import Layout from "../components/Layout";
 
-export default function Index() {
+export default function Nosotros() {
   return (
     <div>
       <Layout>
-        <h2>Desde INDEX</h2>
+        <h2>Desde Nosotros</h2>
       </Layout>
     </div>
   )
