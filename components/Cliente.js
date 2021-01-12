ff2a1f90d33a15adf8a5b1fc604e191c5ebcd09b
Hiram Guerrero <hiramg90@gmail.com>
2021-01-12 17:17:31 -0600

    section 17 completed: clients deletion

---
+++ components/Cliente.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import Swal from 'sweetalert2';
+import {gql, useMutation} from "@apollo/client";
+
+const ELIMINAR_CLIENTE = gql`
+    mutation eliminarCliente($id: ID!){
+        eliminarCliente(id: $id)
+    }
+`;
+
+const OBTENER_CLIENTES_USUARIO = gql`
+    query obtenerClientesVendedor{
+        obtenerClientesVendedor{
+            id
+            nombre
+            apellido
+            empresa
+            email
+        }
+    }
+`;
+
+const Cliente = ({cliente}) => {
+
+  const [eliminarCliente] = useMutation(ELIMINAR_CLIENTE, {
+    update(cache) {
+
+      //Obtener copia del objeto en Cache
+      const {obtenerClientesVendedor} = cache.readQuery({query: OBTENER_CLIENTES_USUARIO});
+
+      // Reescribir el cache
+      cache.writeQuery({
+        query: OBTENER_CLIENTES_USUARIO,
+        data: {
+          obtenerClientesVendedor: obtenerClientesVendedor.filter(clienteActual => clienteActual.id !== id),
+        }
+      });
+
+
+    }
+  });
+
+  const {nombre, apellido, empresa, email, id} = cliente;
+
+  const confirmarEliminarCliente = async (id) => {
+
+    const result = await Swal.fire({
+      title: 'Estás seguro?',
+      text: "Esta acción no se puede deshacer",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, confirmo',
+      cancelButtonText: 'Cancelar',
+    });
+
+    if (result.isConfirmed) {
+      try {
+        const {data} = await eliminarCliente({variables: {id}});
+        console.log(data);
+
+        Swal.fire(
+          'Eliminado!',
+          data.eliminarCliente,
+          'success',
+        )
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+  };
+
+  return (
+    <tr key={id}>
+      <td className="border px-4 py-2">{nombre} {apellido}</td>
+      <td className="border px-4 py-2">{empresa}</td>
+      <td className="border px-4 py-2">{email}</td>
+      <td className="border px-4 py-2">
+        <button
+          onClick={() => confirmarEliminarCliente(id)}
+          className="flex justify-center items-center bg-red-800 text-white py-2 px-4 w-full rounded text-xs uppercase font-bold">
+          Eliminar <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+        </svg></button>
+      </td>
+    </tr>
+  );
+};
+
+export default Cliente;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import Cliente from "../components/Cliente";
 import {gql, useQuery} from '@apollo/client';
 import {useRouter} from "next/router";
 import Link from 'next/link';
@@ -40,7 +41,9 @@ export default function Index() {
         <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
 
         <Link href="/nuevocliente">
-          <a className="bg-blue-800 text-white py-2 px-5 my-3 inline-block rounded text-sm hover:bg-gray-800 uppercase font-bold">Nuevo Cliente</a>
+          <a
+            className="bg-blue-800 text-white py-2 px-5 my-3 inline-block rounded text-sm hover:bg-gray-800 uppercase font-bold">Nuevo
+            Cliente</a>
         </Link>
 
         <table className="table-auto shadow-md mt-10 w-full w-lg">
@@ -49,16 +52,11 @@ export default function Index() {
             <th className="w-1/5 py-2">Nombre</th>
             <th className="w-1/5 py-2">Empresa</th>
             <th className="w-1/5 py-2">Email</th>
+            <th className="w-1/5 py-2">Eliminar</th>
           </tr>
           </thead>
           <tbody className="bg-white">
-          {data.obtenerClientesVendedor.map(cliente => (
-            <tr key={cliente.id}>
-              <td className="border px-4 py-2">{cliente.nombre}</td>
-              <td className="border px-4 py-2">{cliente.empresa}</td>
-              <td className="border px-4 py-2">{cliente.email}</td>
-            </tr>
-          ))}
+          {data.obtenerClientesVendedor.map(cliente => (<Cliente key={cliente.id} cliente={cliente}/>))}
           </tbody>
         </table>
       </Layout>
